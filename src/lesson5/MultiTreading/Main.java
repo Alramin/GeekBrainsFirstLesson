dcab227b3bc9d226689fc6b6d4a5ccb6435ab52e
Alramin <alramin48@gmail.com>
2020-02-13 23:52:35 +0300

    Fifth lesson

---
+++ src/lesson5/MultiTreading/Main.java
@@ -0,0 +1,10 @@
+package lesson5.MultiTreading;
+
+
+public class Main {
+    public static void main(String s[]) {
+        MultiThreading o = new MultiThreading();
+        o.runOneThread();
+        o.runTwoThreads();
+    }
+}
